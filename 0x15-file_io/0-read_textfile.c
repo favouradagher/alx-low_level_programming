5126527272b304859972e5a791a3539c7273e95e
favouradagher <favouradagher@gmail.com>
2023-07-10 08:11:30 -0700

    git

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,55 +4,51 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * read_textfile - Reads a text file and prints its content to the standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd; /* file descriptor */
+	ssize_t n_read, n_wrote;
 	char *buffer;
-	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
+	/* open */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	/* malloc buffer */
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	/* read */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	buffer[bytes_read] = '\0';
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	/* write */
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (n_wrote == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	free(buffer);
 	close(fd);
+	return (n_read);
 
-	return (bytes_written);
 }
-
