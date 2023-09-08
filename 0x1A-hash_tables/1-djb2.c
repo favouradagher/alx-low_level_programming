66277388a2f4081953a2f4e7168a165881bb64cc
favour adagher <favouradagher@gmail.com>
2023-09-08 02:49:36 -0700

    C - Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,36 +1,19 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
  *
- * Return: A pointer to the newly created hash table, or NULL if failure.
+ * Return: The calculated hash.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+unsigned long int hash_djb2(const unsigned char *str)
 {
-    hash_table_t *new_table;
-    unsigned long int i;
+	unsigned long int hash;
+	int c;
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-    /* Allocate memory for the array of hash_node_t pointers */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
-
-    /* Initialize each element of the array to NULL */
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
-
-    new_table->size = size;
-
-    return (new_table);
+	return (hash);
 }
-
